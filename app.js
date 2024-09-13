b65d3dd8252112bf0fb53710cb00bcc5765e9ae8
sumitpuri00 <sumitpuri006@gmail.com>
2024-09-13 20:27:43 +0530

    pushsumi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -36,7 +36,7 @@ app.get('/',(req,res)=>{
 
 
 
-server.listen(port,(err)=>{
-    console.log(`Server is running on port ${port}`);
+server.listen(3000,(err)=>{
+    console.log(`Server is running on port 3000`);
     
 })
